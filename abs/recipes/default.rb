37a117344a9c334d95b546c9b58f00e9ea02bd06
root <root@server1.abc.com>
2017-04-12 08:09:00 +0530

    added cookbokks from workstation

---
+++ abs/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: abs
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template "/etc/abs" do
+          source "abs.erb"
+end
